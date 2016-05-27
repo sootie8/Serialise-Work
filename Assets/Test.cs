7fcb192784d6772703cf997a695f175cae935b09
sootie8 <jordancarter@MacBook-Pro.local>
2016-05-27 18:10:35 +0100

    Added Dictionary To Array Tests

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,6 +4,7 @@ using System.Xml.Serialization;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using System.Linq;
 
 public class Test : MonoBehaviour {
 
@@ -13,10 +14,15 @@ public class Test : MonoBehaviour {
 
 	private SomeStruct someStruct;
 
+	private Dictionary<int, float> dictionary;
+
 	public void OnEnable()
 	{
 		someClass = new SomeClass(1f);
 		someStruct = new SomeStruct(1f);
+		dictionary = new Dictionary<int, float>();
+		dictionary.Add(1, 2f);
+		dictionary.Add(3, 4f);
 	}
 	// Update is called once per frame
 	void Update () 
@@ -31,15 +37,34 @@ public class Test : MonoBehaviour {
 			Debug.Log(typeof(SomeStruct).GetProperty("SomeValue").GetValue(someStruct, null));
 		
 		}
+		//Base line test, this should crash.
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			var arr = dictionary.ToArray();
+			Debug.Log(arr[0]);
+		}
 
-		if (Input.GetKeyDown(KeyCode.Y))
+		//IEnumerable foreach.
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			var pairs = new KeyValuePair<int, float>[dictionary.Count];
+			int i = 0;
+			foreach (var keypair in dictionary)
+			{
+				pairs[i] = keypair;
+				i++;
+			}
+			Debug.Log(pairs[0]);
+		}
+
+		/*if (Input.GetKeyDown(KeyCode.Y))
 		{
 			var keyValuePairs = new KeyValuePair<int, float>[2]{new KeyValuePair<int, float>(1, 2f), new KeyValuePair<int, float>(3, 4f)};
 			var stream = new MemoryStream(); 
 			var formatter = new BinaryFormatter();
 			formatter.Serialize(stream, keyValuePairs);
 			Debug.Log(stream.Length);
-		}
+		}*/
 	}
 
 	public class SomeClass
